b4dd8c2b40d060ad6d150cd6ccd367003c4b1be1
unknown <ngocduc.2109@gmail.com>
2021-04-20 10:11:00 +0700

    [Bài tập] Cài đặt thuật toán tìm kiếm nhị phân sử dụng đệ quy

---
+++ RecursiveBinarySearch/src/Search.java
@@ -0,0 +1,29 @@
+public class Search {
+
+    public static int RecursiveBinarySearch(int[] array, int first, int last, int value){
+        int middle;
+        while (first <= last){
+            middle = (first + last)/2;
+            if (value < array[middle]){
+                return RecursiveBinarySearch(array, first, middle - 1, value);
+            } else if (value > array[middle]){
+                return RecursiveBinarySearch(array, middle + 1, last, value);
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+        int first = 0;
+        int last = array.length - 1;
+        System.out.println(RecursiveBinarySearch(array, first, last, 2));   /* 0 */
+        System.out.println(RecursiveBinarySearch(array, first, last, 11));  /* 4 */
+        System.out.println(RecursiveBinarySearch(array, first, last, 79));  /*12 */
+        System.out.println(RecursiveBinarySearch(array, first, last, 1));   /*-1 */
+        System.out.println(RecursiveBinarySearch(array, first, last, 5));   /*-1 */
+        System.out.println(RecursiveBinarySearch(array, first, last, 80));  /*-1 */
+    }
+}
